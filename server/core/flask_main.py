5206e7e1d93362d3f1aa97a730515017f54eceba
MrDrovE <ilyxa.eletskiya@gmail.com>
2023-02-23 18:58:09 +0400

    add_recipes
    я хз как это должно работать , памагите

--- server/core/flask_main.py
+++ server/core/flask_main.py
@@ -20,19 +20,6 @@ session = Session()
 
 migrate = Migrate(app, engine)
 
-""" Пример json для добавления"""
-recipe = {
-    "user": {
-        "recipe": [
-            {"text": "text"},
-            {"text": "картинка #1"},
-            {"text": "text"},
-        ],
-        "comments": [],
-        "likes": [],
-    }
-}
-
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
@@ -50,19 +37,36 @@ def user_reg():
     if user:
         return make_response("User alredy exist")
     if name and surname and email and password:
-        new_user = User(name=name, surname=surname, email=email, password=User.set_password(password),
-                        created_date=time())
+        new_user = User(name=name, surname=surname, email=email, password=User.set_password(password))
         session.add(new_user)
         session.commit()
     else:
-        return abort(400)
+        abort(400)
 
 
 # Добавить рецепт
 @app.route('/api/add_recipes/<int:user_id>', methods=['POST'])
 def add_recipes(user_id):
-    pass
-
+    if not request.json:
+        abort(400)
+    title = request.json["title"]
+    category = request.json["category"]
+    calories = request.json["calories"]
+    proteins = request.json["proteins"]
+    fats = request.json["fats"]
+    carbohydrates = request.json["carbohydrates"]
+    ingredients = request.json["ingredients"]
+    user = User.query.filter_by(id=user_id).first()
+    if user:
+        new_recipe = Recipe(title=title, category=category, time=time(),
+                            calories=calories, proteins=proteins, fats=fats,
+                            carbohydrates=carbohydrates)
+        session.add(new_recipe)
+        session.commit()
+        if ingredients:
+            new_recipe.ingredients.extend(ingredients)
+            session.commit()
+    return make_response("Successful add")
 
 # Удалить рецепт
 @app.route('/api/rem_recipes', methods=['DELETE'])
@@ -79,6 +83,8 @@ def edit_recipes():
 # Получить рецепт
 @app.route('/api/get_recipes', methods=['GET'])
 def get_recipes():
+    recipe = session.query(Recipe).all()
+    print(recipe)
     return jsonify({'recipe': recipe})
 
 
